87b6c5eee77ff612be34623347c644533f433a71
Theshakyhorse <amaximus.sierra@gmail.com>
2026-02-22 16:21:44 -0800

    second

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
 //COMSC-210 | Lab 12 | Alexander Sierra
 #include <iostream>
+#include <algorithm>
+#include <numeric> 
+#include <array>
 using namespace std;
 
+//number of players on a team is typically constant
+const int SIZE = 30;
+
 int main() {
-    
+    array<double, SIZE> heights;
+    cout << "There are " << SIZE << "people on this team" << endl;
+    cout << "please enter the heights for the players: " << endl;
+    for(int i = 0; i < SIZE; i++) {
+        cin >> heights[i];
+    }
     return 0;
 }
