dd3c24b30fe6711e066954f137ecdb932053f32b
Theshakyhorse <amaximus.sierra@gmail.com>
2026-02-22 17:03:11 -0800

    final

--- main.cpp
+++ main.cpp
@@ -15,34 +15,42 @@ int main() {
     cout << "There are " << SIZE << "people on this team" << endl;
     cout << "please enter the heights for the players" << endl; 
     cout << "For example, entering 6 or 6.0 = the player is 6 ft tall" << endl;
+    //reads data to get player heights
     for(int i = 0; i < SIZE; i++) {
         cout << "player " << i+1 << endl;
         cin >> heights[i];
     }
 
     cout << endl << "Outputting details: " << endl;
+    //checks if heights were entered otherwise just state that nothing was entered
     if (heights.empty() == 0){
-        cout << "Your first input was " << heights.front() << endl;
-        cout << "Your last input was " << heights.back() << endl;
+        cout << "The first player's height was " << heights.front() << endl;
+        cout << "The last player's height was " << heights.back() << endl;
         cout << "their heights' sorted from least to greatest is ";
+        //sorts data from least to greatest then outputs
         sort(heights.begin(), heights.end());
         for(int i = 0; i < SIZE; i++){
             cout << "player " << i+1 << "'s height: " << heights[i];
         }
         cout << endl;
+
         cout << "their heights' sorted from greatest to least is ";
+        //sorts data from greatest to least then outputs
         sort(heights.rbegin(), heights.rend());
         for(int i = 0; i < SIZE; i++){
             cout << "player " << i+1 << "'s height: " << heights[i];
         }
         cout << endl;
+        //extremes
         cout << "shortest height: " << *min_element(heights.begin(), heights.end()) << endl;
-        
+        cout << "tallest height: " << *max_element(heights.begin(), heights.end()) << endl;
+
     }
     else{
-        cout << "you entered nothing" << endl;
+        cout << "No player heights have been entered" << endl;
     }
 
+    //code to find a certain height gotten from user input
     cout << "enter a height: ";
     cin >> height;
     array<double, SIZE>::iterator it;
@@ -51,7 +59,7 @@ int main() {
         cout << "player " << it-heights.begin() << " has the same height" << endl;
     }
     else{
-        cout << "no player matches the height of " << *it << " ft" << endl;
+        cout << "no player matches the height of " << *it << endl;
     }
 
     return 0;
