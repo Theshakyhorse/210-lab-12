ec3eb6824c565798ec022f6745cab7e01890a9ec
Theshakyhorse <amaximus.sierra@gmail.com>
2026-02-22 16:31:54 -0800

    third

--- main.cpp
+++ main.cpp
@@ -11,9 +11,18 @@ const int SIZE = 30;
 int main() {
     array<double, SIZE> heights;
     cout << "There are " << SIZE << "people on this team" << endl;
-    cout << "please enter the heights for the players: " << endl;
+    cout << "please enter the heights for the players" << endl; 
+    cout << "For example 6 or 6.0 means the player is 6 feet tall " << endl;
     for(int i = 0; i < SIZE; i++) {
+        cout << "player " << i+1 << endl;
         cin >> heights[i];
     }
+    cout << endl << "Outputting details: " << endl;
+    if (heights.empty() == 0){
+        cout << "Your first input was " << heights.front() << endl;
+        cout << "Your last input was " << heights.back() << endl;
+    }
+    else{}
+
     return 0;
 }
